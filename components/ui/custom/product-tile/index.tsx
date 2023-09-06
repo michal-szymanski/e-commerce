807d8fe83bfda4bd168a1491cd6fc712971af81d
Michał Szymański <michal.szymanski92@gmail.com>
2023-09-06 18:38:29 +0200

    Fix semantic and accesibility for product tile

--- components/ui/custom/product-tile/index.tsx
+++ components/ui/custom/product-tile/index.tsx
@@ -13,17 +13,19 @@ const ProductTile = ({ product }: Props) => {
     const dispatch = useDispatch();
 
     return (
-        <div
+        <article
             className="hover:shadow-product-tile flex cursor-pointer flex-col rounded-md bg-white p-5 transition-shadow"
             onClick={() => dispatch(addToCart(product))}
+            role="button"
+            tabIndex={0}
         >
             <Image src={product.src} alt={product.name} width={200} height={200} />
-            <div className="mt-3 flex flex-col gap-2 px-2">
+            <header className="mt-3 flex flex-col gap-2 px-2">
                 <h3 className="font-semibold">{product.name}</h3>
                 <Rating value={getRandomValue(1, 5)} count={getRandomValue(1, 100)} />
                 <span className="size text-xl font-bold">{product.price} PLN</span>
-            </div>
-        </div>
+            </header>
+        </article>
     );
 };
 
