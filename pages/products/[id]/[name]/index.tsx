653661dc095e05c2bd96e6259577ac0729f92cc4
Michał Szymański <michal.szymanski92@gmail.com>
2023-09-14 11:20:34 +0200

    fix previous commit (missing product page)

---
+++ pages/products/[id]/[name]/index.tsx
@@ -0,0 +1,19 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { QueryClient } from '@tanstack/react-query';
+import { z } from 'zod';
+import { getProduct } from '@/sql-service';
+
+export default ({ product }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+    return <div>{JSON.stringify(product)}</div>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const parsedId = z.coerce.number().parse(context.query.id);
+    const product = await getProduct(parsedId);
+
+    return {
+        props: {
+            product
+        }
+    };
+};
