203b1ed17acd8a7f0be7f69006cf2a8e54bd68ce
Michał Szymański <michal.szymanski92@gmail.com>
2023-09-16 00:32:38 +0200

    fix cart footer

--- components/layouts/cart-footer/index.tsx
+++ components/layouts/cart-footer/index.tsx
@@ -1,17 +1,16 @@
 import { Button } from '@/components/ui/button';
 import { z } from 'zod';
 import { cn, getTotalPrice } from '@/lib/utils';
-import { useSelector } from 'react-redux';
-import { RootState } from '@/store';
+import { useCart } from '@/hooks/queries';
 
 const CartFooter = () => {
-    const cart = useSelector((state: RootState) => state.order.cart);
-    const totalPrice = cart.reduce((acc, curr) => acc + z.coerce.number().parse(getTotalPrice(curr.product, curr.quantity)), 0).toFixed(2);
+    const { data: cart } = useCart();
+    const totalPrice = cart?.reduce((acc, curr) => acc + z.coerce.number().parse(getTotalPrice(curr.product, curr.quantity)), 0).toFixed(2);
 
     return (
         <footer
             className={cn('fixed bottom-0 flex h-28 w-full translate-y-full items-center justify-end gap-5 bg-white p-10 shadow-2xl transition-transform', {
-                'translate-y-0': cart.length > 0
+                'translate-y-0': cart?.length
             })}
         >
             <span className="text-2xl font-bold">{totalPrice} zł</span>
