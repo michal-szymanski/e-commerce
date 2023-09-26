90bfb60d8e1b980c8f5a93ec2be4cf4801de6496
Michał Szymański <michal.szymanski92@gmail.com>
2023-09-26 17:29:56 +0200

    Confetti

--- pages/order-confirmation/[sessionId]/index.tsx
+++ pages/order-confirmation/[sessionId]/index.tsx
@@ -4,37 +4,54 @@ import stripe from '@/stripe';
 import Link from 'next/link';
 import { getAuth, clerkClient } from '@clerk/nextjs/server';
 import { Button } from '@/components/ui/button';
+import Confetti from 'react-confetti';
+import { useEffect, useState } from 'react';
 
 export default ({ session, firstName }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+    const [{ width, height }, setDimensions] = useState({ width: 0, height: 0 });
+
+    useEffect(() => {
+        const eventHandler = () => {
+            setDimensions({ width: window.innerWidth, height: innerHeight });
+        };
+        eventHandler();
+        window.addEventListener('resize', eventHandler);
+        return () => window.removeEventListener('resize', eventHandler);
+    }, []);
+
     const orderId = session.metadata.orderId;
+
     return (
-        <div className="container grid h-[60%] place-items-center">
-            <div>
-                <h2 className="text-3xl font-bold">Thank you, {firstName}!</h2>
-                <p className="py-5 text-xl">
-                    Your order number{' '}
-                    {
-                        <Link href={`/orders/${orderId}`} className="font-bold underline">
-                            {orderId}
-                        </Link>
-                    }{' '}
-                    has been created.
-                </p>
-                <p className="pb-2 text-lg">
-                    You should receive an order confirmation email shortly. If the email hasn't arrived within few minutes, please check your spam folder to see
-                    if the email was routed there.
-                </p>
-                <p className="text-lg">
-                    Feel free to close this page or head back to the{' '}
-                    {
-                        <Link href="/">
-                            <Button>Home Page</Button>
-                        </Link>
-                    }{' '}
-                    .
-                </p>
+        <>
+            <Confetti width={width} height={height} numberOfPieces={500} recycle={false} />
+            <div className="container grid h-[60%] place-items-center">
+                <div>
+                    <h2 className="text-3xl font-bold">Thank you, {firstName}!</h2>
+                    <p className="py-5 text-xl">
+                        Your order number{' '}
+                        {
+                            <Link href={`/orders/${orderId}`} className="font-bold underline">
+                                {orderId}
+                            </Link>
+                        }{' '}
+                        has been created.
+                    </p>
+                    <p className="pb-2 text-lg">
+                        You should receive an order confirmation email shortly. If the email hasn't arrived within few minutes, please check your spam folder to
+                        see if the email was routed there.
+                    </p>
+                    <p className="text-lg">
+                        Feel free to close this page or head back to the{' '}
+                        {
+                            <Link href="/">
+                                <Button>Home Page</Button>
+                            </Link>
+                        }{' '}
+                        .
+                    </p>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
