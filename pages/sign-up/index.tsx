996a2ea5f54416b6d1cb60168ba4c93749905110
Michał Szymański <michal.szymanski92@gmail.com>
2023-09-27 21:40:07 +0200

    animate summary errors

--- pages/sign-up/index.tsx
+++ pages/sign-up/index.tsx
@@ -8,6 +8,7 @@ import { getAuth } from '@clerk/nextjs/server';
 import CodeVerificationForm from '@/components/ui/custom/forms/code-verification-form';
 import SignUpForm from '@/components/ui/custom/forms/sign-up-form';
 import { cn } from '@/lib/utils';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default () => {
     const [summaryErrors, setSummaryErrors] = useState<{ id: string; message: string }[]>([]);
@@ -18,21 +19,25 @@ export default () => {
             <Head>
                 <title>{`Sign Up | ${env.NEXT_PUBLIC_APP_NAME}`}</title>
             </Head>
-            <div className="container flex h-3/5 flex-col items-center justify-end">
-                <div className="flex w-96 flex-col gap-5">
-                    {summaryErrors.length > 0 && (
-                        <Alert className="text-destructive">
-                            <ExclamationCircleIcon className="h-5 w-5 !text-destructive" />
-                            <AlertTitle>Heads up!</AlertTitle>
-                            <AlertDescription>
-                                {summaryErrors.map((e) => (
-                                    <p key={e.id} className="text-sm font-medium">
-                                        {e.message}
-                                    </p>
-                                ))}
-                            </AlertDescription>
-                        </Alert>
-                    )}
+            <div className="container flex h-3/5 w-[400px] flex-col items-center justify-end">
+                <div className="flex w-[400px] flex-col gap-5">
+                    <AnimatePresence>
+                        {summaryErrors.length > 0 && (
+                            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} key="summary-errors">
+                                <Alert className="text-destructive">
+                                    <ExclamationCircleIcon className="h-5 w-5 !text-destructive" />
+                                    <AlertTitle>Heads up!</AlertTitle>
+                                    <AlertDescription>
+                                        {summaryErrors.map((e) => (
+                                            <p key={e.id} className="text-sm font-medium">
+                                                {e.message}
+                                            </p>
+                                        ))}
+                                    </AlertDescription>
+                                </Alert>
+                            </motion.div>
+                        )}
+                    </AnimatePresence>
                     <div className="w-[400px] overflow-hidden">
                         <div
                             className={cn('grid grid-cols-[repeat(2,_400px)] transition-transform', {
