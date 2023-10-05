b4b8d3688a7499685dc15819ebec59ab6db2ffc1
Michał Szymański <michal.szymanski92@gmail.com>
2023-10-05 18:25:46 +0200

    fix bottom space for footer

--- components/layouts/default-layout/index.tsx
+++ components/layouts/default-layout/index.tsx
@@ -3,20 +3,35 @@ import Navbar from '@/components/layouts/navbar';
 import CartFooter from '@/components/layouts/cart-footer';
 import { env } from '@/env.mjs';
 import Head from 'next/head';
+import { useCart } from '@/hooks/queries';
+import { useOrganization, useUser } from '@clerk/nextjs';
+import { cn } from '@/lib/utils';
 
 type Props = {
     children: ReactNode;
 };
 
-const Layout = ({ children }: Props) => (
-    <>
-        <Head>
-            <title>{env.NEXT_PUBLIC_APP_NAME}</title>
-        </Head>
-        <Navbar />
-        <main className="absolute bottom-28 left-0 right-0 top-[70px] overflow-y-auto pt-[100px]">{children}</main>
-        <CartFooter />
-    </>
-);
+const Layout = ({ children }: Props) => {
+    const { isSignedIn } = useUser();
+    const { organization } = useOrganization();
+    const { data: cart } = useCart(!!isSignedIn && !organization);
+
+    return (
+        <>
+            <Head>
+                <title>{env.NEXT_PUBLIC_APP_NAME}</title>
+            </Head>
+            <Navbar />
+            <main
+                className={cn('absolute bottom-0 left-0 right-0 top-[70px] overflow-y-auto pt-[100px]', {
+                    'bottom-28': cart && cart.length > 0
+                })}
+            >
+                {children}
+            </main>
+            <CartFooter />
+        </>
+    );
+};
 
 export default Layout;
