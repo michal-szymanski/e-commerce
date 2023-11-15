c11f4a0cb3e9e8881b5a676f9fa0834c041cc5a8
Michał Szymański <michal.szymanski92@gmail.com>
2023-11-15 17:29:33 +0100

    Settings/Notifications

--- pages/settings/notifications/index.tsx
+++ pages/settings/notifications/index.tsx
@@ -1,8 +1,27 @@
 import { ReactNode } from 'react';
 import SettingsLayout from '@/components/layouts/settings-layout';
+import { Separator } from '@/components/ui/separator';
+import { Switch } from '@/components/ui/switch';
 
 const Page = () => {
-    return <div>Notifications</div>;
+    return (
+        <section>
+            <header>
+                <h3 className="text-lg font-medium">Notifications</h3>
+                <p className="text-sm text-muted-foreground">Manage your notifications.</p>
+                <div className="py-6">
+                    <Separator />
+                </div>
+                <article className="flex items-center justify-between rounded-lg border p-4">
+                    <div>
+                        <h4 className="font-bold">Mandatory notifications</h4>
+                        <p className="text-sm text-muted-foreground">Includes security and order processing emails.</p>
+                    </div>
+                    <Switch checked={true} disabled />
+                </article>
+            </header>
+        </section>
+    );
 };
 
 Page.getLayout = (page: ReactNode) => {
