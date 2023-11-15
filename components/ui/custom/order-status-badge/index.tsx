ffaecde660eb630ac662d88feaa83314767b9e69
Michał Szymański <michal.szymanski92@gmail.com>
2023-11-15 23:44:50 +0100

    colorful badges

--- components/ui/custom/order-status-badge/index.tsx
+++ components/ui/custom/order-status-badge/index.tsx
@@ -6,8 +6,19 @@ type Props = {
     status: z.infer<typeof orderStatusSchema>;
 };
 
+type Variant = 'default' | 'blue' | 'yellow' | 'green' | 'red';
+
+const statusVariantMap: Record<z.infer<typeof orderStatusSchema>, Variant> = {
+    Pending: 'default',
+    New: 'blue',
+    'In Progress': 'yellow',
+    Completed: 'green',
+    Cancelled: 'red'
+};
+
 const OrderStatusBadge = ({ status }: Props) => {
-    return <Badge variant="outline">{status}</Badge>;
+    const variant = statusVariantMap[status];
+    return <Badge variant={variant}>{status}</Badge>;
 };
 
 export default OrderStatusBadge;
